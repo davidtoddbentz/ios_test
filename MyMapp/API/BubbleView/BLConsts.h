377db298deff5bf7ff0efcc8909d4756a2cf49dc
David Bentz <davidtoddbentz@gmail.com>
2017-09-22 11:05:19 -0400

    simpler project

--- MyMapp/API/BubbleView/BLConsts.h
+++
@@ -1,9 +0,0 @@
-#ifndef BLConsts_h
-#define BLConsts_h
-
-typedef NS_ENUM(NSInteger, BLBubbleNodeStateCount) {
-    BLBubbleNodeStateCountFirst = BLBubbleNodeStateRemoved,
-    BLBubbleNodeStateCountLast = BLBubbleNodeStateHighlighted
-};
-
-#endif /* BLConsts_h */
---
+++ ios_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios_test
+//
+//  Created by David Bentz on 9/22/17.
+//  Copyright © 2017 David Bentz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
